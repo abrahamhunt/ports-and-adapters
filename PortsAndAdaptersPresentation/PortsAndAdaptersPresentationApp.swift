e69c336c61538426ad7c73e39241c6adbf3adec6
Abraham Hunt <abrahamhunt@gmail.com>
2025-07-18 07:51:13 -0600

    Initial files and excluded .DS_store

---
+++ PortsAndAdaptersPresentation/PortsAndAdaptersPresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortsAndAdaptersPresentationApp.swift
+//  PortsAndAdaptersPresentation
+//
+//  Created by Abe Hunt on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PortsAndAdaptersPresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PortsAndAdaptersPresentationTests/PortsAndAdaptersPresentationTests.swift
@@ -0,0 +1,17 @@
+//
+//  PortsAndAdaptersPresentationTests.swift
+//  PortsAndAdaptersPresentationTests
+//
+//  Created by Abe Hunt on 7/17/25.
+//
+
+import Testing
+@testable import PortsAndAdaptersPresentation
+
+struct PortsAndAdaptersPresentationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
